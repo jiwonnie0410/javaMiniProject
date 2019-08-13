7c18c1aac820f2f72e1c25b7ec82e5e99a70b2d1
User <User@YD02-02>
2019-08-13 16:46:42 +0900

    16:46

--- src/javaMiniProject/view/InformController.java
+++ src/javaMiniProject/view/InformController.java
@@ -15,44 +15,39 @@ import javafx.scene.control.TextField;
 import javafx.scene.layout.AnchorPane;
 
 public class InformController implements Initializable {
-	@FXML private AnchorPane achorPane;
-	@FXML private Button btnDelete;
-	@FXML private Button btnUpdate;
-	@FXML private TextField txtStudentNumber;
-	@FXML private TextField txtMajorName;
-	@FXML private TextField txtStudentName;
-	@FXML private TextField txtCollegeName;
-	@FXML private TextField txtAddress;
-	@FXML private TextField txtPhone;
-	@FXML private TextField txtIdentification;
-	@FXML private TextField txtGender;
+	@FXML	private AnchorPane achorPane;
+	@FXML	private Button btnDelete;
+	@FXML	private Button btnUpdate;
+	@FXML	private TextField txtStudentNumber;
+	@FXML	private TextField txtMajorName;
+	@FXML	private TextField txtStudentName;
+	@FXML	private TextField txtCollegeName;
+	@FXML	private TextField txtAddress;
+	@FXML	private TextField txtPhone;
+	@FXML	private TextField txtIdentification;
+	@FXML	private TextField txtGender;
 //	@FXML private ComboBox comboGender;
-	
-	
+
 	@FXML
 	private void selectBasicInform(ActionEvent actionEvent) {
-	
+
 	}
-@Override
-public void initialize(URL arg0, ResourceBundle arg1) {
-	// TODO Auto-generated method stub
-	// 학번 이름 폰번호 주소 전공 주민번호 성별
-	List<Map<String, Object>> list = new ArrayList<>();
-	list = StudentsInformServiceImpl.getInstance().basicInform(1001);
-	txtStudentNumber.setText(list.get(0).get("studentNumber").toString());
-	txtStudentName.setText(list.get(0).get("studentName").toString());
-	txtPhone.setText(list.get(0).get("phone").toString());
-	txtAddress.setText(list.get(0).get("address").toString());
-	txtMajorName.setText(list.get(0).get("majorName").toString());
-	txtIdentification.setText(list.get(0).get("identification").toString());
-	System.out.println(list.get(0).get("identification").toString());
+
+	@Override
+	public void initialize(URL arg0, ResourceBundle arg1) {
+		// TODO Auto-generated method stub
+		// 학번 이름 폰번호 주소 전공 주민번호 성별
+		List<Map<String, Object>> list = new ArrayList<>();
+		list = StudentsInformServiceImpl.getInstance().basicInform(1001);
+		txtStudentNumber.setText(list.get(0).get("studentNumber").toString());
+		txtStudentName.setText(list.get(0).get("studentName").toString());
+		txtPhone.setText(list.get(0).get("phone").toString());
+		txtAddress.setText(list.get(0).get("address").toString());
+		txtMajorName.setText(list.get(0).get("majorName").toString());
+		txtIdentification.setText(list.get(0).get("identification").toString());
+		System.out.println(list.get(0).get("identification").toString());
 //	comboGender.setItem(list.get(6).get("gender").toString());
-	
-}
+
+	}
 
 }
-
-
-
-
-
